968a923354ed318e253a761c1f1300676b9c77f1
Erskine Gamaliel L. Duenas <erskine.duenas@gmail.com>
2024-08-06 12:59:28 +0800

    added new avatars

--- src/avatar/Avatar.js
+++ src/avatar/Avatar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import avatarImage from "../assets/avatar-image.png";
+import avatarImage from "../assets/ers_avatar.png";
 import "../styles/avatar.css";
 
 const Avatar = ({ page }) => {
